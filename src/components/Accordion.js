fda974309b7e48457a6b6d8fc0028308f8ea55ea
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-07-20 21:55:24 +0530

    expanding accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -7,15 +7,18 @@ const Accordion = ({items}) => {
 
     const onTitleClicked = (index) => {
         setActiveIndex(index);
-    }
+    };
 
     const renderedItems = items.map((item, index) => {
+
+        const active = index === activeIndex ? 'active' : '';
+
         return <React.Fragment key={item.title}>
-            <div className="title active" onClick={()=> onTitleClicked(index)}>
+            <div className={`title ${active}`} onClick={()=> onTitleClicked(index)}>
                 <i className="dropdown icon"></i>
                 {item.title}
             </div>
-            <div className="content active">
+            <div className={`content ${active}`}>
                 <p>{item.content}</p>
             </div>
         </React.Fragment>
@@ -23,7 +26,6 @@ const Accordion = ({items}) => {
 
     return <div className="ui styled accordion">
         {renderedItems}
-        <h1>{activeIndex}</h1>
     </div>
 };
 
