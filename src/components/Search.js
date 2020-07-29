88b429e94dd93bd64d09a988e15a303737545e6d
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-07-29 11:57:25 +0530

    linkin a wikipedia page

--- src/components/Search.js
+++ src/components/Search.js
@@ -27,15 +27,21 @@ const Search = () => {
     }, [term]);
 
     const renderResults = results.map((result) => {
-        return <div key={result.pageid} className="item">
+        return (
+        <div key={result.pageid} className="item">
+            <div className="right floated content">
+                <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+            </div>
             <div className="content">
                 <div className="header">
                     {result.title}
                 </div>
-                {result.snippet}
+                <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+                
             </div>
         </div>
-    })
+        );
+    });
 
     return (
         <div>
